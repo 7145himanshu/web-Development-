794ca24d8a1acd21e1e9e1178c26a71e9644b2b9
7145himanshu <123812713+7145himanshu@users.noreply.github.com>
2024-05-03 15:22:02 +0530

    Add files via upload

---
+++ APPLE CLONE/apple.js
@@ -0,0 +1,19 @@
+console.log("hello")
+let c="whatsupp"
+
+console.log(c)
+
+
+// console.log(a+b)
+
+
+let a=10;   //let is most recommended 
+
+const b=11 // not updated nor redeclared 
+var d = null;
+
+
+{
+     let h=10;
+     console.log(h) 
+}
